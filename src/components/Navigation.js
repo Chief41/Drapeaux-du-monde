cd0cde4f29a04f5feca3967e0bb2eba1d4d63c7f
Serdar Olmez <serdarolmez0@gmail.com>
2024-02-28 23:37:08 +0100

    countries

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-    return (
-        <div className='navigation'>
-            <NavLink to="/"><li>Accueil</li></NavLink>
-            <NavLink to="/about"><li>A propos</li></NavLink>
-        </div>
-    );
+  return (
+    <div className="navigation">
+      <ul>
+        <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+          <li>Accueil</li>
+        </NavLink>
+        <NavLink to="/about"className={(nav) => (nav.isActive ? "nav-active" : "")}>
+          <li>A propos</li>
+        </NavLink>
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
