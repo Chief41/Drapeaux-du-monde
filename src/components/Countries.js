aec43596b8060e22daa05fd605bff4a8375e28e6
Serdar Olmez <serdarolmez0@gmail.com>
2024-02-29 01:24:02 +0100

    finir les input

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect,useState } from 'react';
+import Card from './Card';
 
 const Countries = () => {
+    const [data, setData] = useState([])
+/* Le useEffect se joue quand le composant est monté */
+    useEffect(() => {
+        axios
+        .get ("https://restcountries.com/v3.1/all")
+        .then((res) => setData(res.data));
+    }, [])
+
     return (
-        <div>
+        <div className='countries'>
+            <ul className="radio-container">
+                <input type="range" min='1' max='250'/>
+            </ul>
            <h1>COUNTRIES</h1> 
+           <ul>
+            {data.map((country, index) => (
+            <Card key={index} country={country} />
+            ))}
+           </ul>
         </div>
     );
 };
